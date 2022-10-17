a61cd6be3d032e979770bd6b4bef66a98efcb44a
Jonathan Kao <83883817+kaojonathan@users.noreply.github.com>
2022-10-17 15:25:13 -0400

    replaced login default with axios post to backend

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,15 +2,13 @@ import React, { useState } from 'react'
 import FrontPage from './FrontPage'
 import LoginForm from './components/LoginForm'
 import './App.css'
+import axios from 'axios'
 
 
 export default function Login() {
-  // move to db
-  const adminUser = {
-    email: 'test@test.com',
-    password: 'test123'
-  }
- 
+
+  
+  const backendUrl = 'http://localhost:5000'
   const [user, setUser] = useState({name: '', email: ''})
   const [error, setError] = useState('');
   
@@ -18,18 +16,42 @@ export default function Login() {
   const Login = details => {
     // console.log(details)
 
-    // TODO: replace this with api call to backend - axios? - remove this admin user stuff
-    if (details.email === adminUser.email && details.password === adminUser.password) {
-      console.log('Admin logged in')
-      setUser({
-        name: details.name,
-        email: details.email
-      })
-      // Replace with regular user login
-    } else {
-      console.log('Login failed')
-      setError('Bad Login Credentials!')
-    }
+    // if (details.email === adminUser.email && details.password === adminUser.password) {
+    //   console.log('Admin logged in')
+    //   setUser({
+    //     name: details.name,
+    //     email: details.email
+    //   })
+    //   // Replace with regular user login
+    // } else {
+    //   console.log('Login failed')
+    //   setError('Bad Login Credentials!')
+    // }
+
+    axios.post(`${backendUrl}/login`, {
+      name: details.name,
+      email: details.email,
+      password: details.password
+    })
+    .then(function (response) {
+      console.log(response);
+      if (response.data === 'success') {
+        console.log(`User ${details.name} logged in`)
+        setUser({
+          name: details.name,
+          email: details.email
+        })
+      } else {
+        console.log(`Login failed for: ${details.name}`)
+        setError('Bad Login Credentials!')
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+      console.log(`Login failed for: ${details.name}`)
+      setError(`Login Failed: ${error}`)
+    });
+
   }
 
   const Logout = () => {
