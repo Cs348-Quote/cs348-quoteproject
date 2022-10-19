c227ac522714b9cd28ac25526925c85c494709ad
Samuel Pei <84410175+iepmas@users.noreply.github.com>
2022-10-19 17:49:55 -0400

    created user_info sql table

---
+++ user_infotable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS "user_info" (
+	"name" TEXT NOT NULL,
+	"email" TEXT NOT NULL PRIMARY KEY,
+	"password" TEXT NOT NULL
+);
+
+INSERT INTO user_info VALUES
+('admin', 'admin@gmail.com', 'admin'),
+('test', 'test@gmail.com', 'test')
