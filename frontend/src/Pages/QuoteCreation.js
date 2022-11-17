95797634bdeb4f3a768ada4c42b1c3ed22fe4374
blaicheo <78671393+blacheo@users.noreply.github.com>
2022-11-16 23:34:26 -0500

    Added category combo box

--- frontend/src/Pages/QuoteCreation.js
+++ frontend/src/Pages/QuoteCreation.js
@@ -1,13 +1,22 @@
+import { categories } from "../components/categories"
 import NavBar from "../components/NavBar"
 import { FormGroup } from "../components/user_info"
 
 function QuoteCreation({CreateQuote}) {
+    const showCategories = categories.map((category) => <option key={category}>{category}</option>)
     return (
         <div>
             <NavBar/>
             <h1>Quote Creator</h1>
             <p>Your thought provoking quote:</p>
-            <textarea autoFocus="true"></textarea>
+            <textarea autoFocus={true}></textarea>
+            <div>
+            <p>What Category does your Quote belong to?</p>
+            <select name="Category">{showCategories}</select>
+            </div>
+            
+            
+            <p>Click Here to Submit</p>
             <button onClick={CreateQuote}>Submit</button>
         </div>
     )
