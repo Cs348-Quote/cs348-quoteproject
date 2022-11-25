7ace8a629e68db40508765a50121dd643a008e12
Jonathan Kao <83883817+kaojonathan@users.noreply.github.com>
2022-11-25 13:56:01 -0500

    building quote map - requires marker rerendering and request logic

---
+++ frontend/src/Pages/QuoteMap.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react'
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  ZoomableGroup,
+  Marker
+} from 'react-simple-maps';
+import { CountryDropdown } from 'react-country-region-selector';
+import NavBar from '../components/NavBar';
+import '../Pages/QuoteMap.css'
+
+const geoUrl =
+  'https://cors-anywhere.herokuapp.com/https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json';
+
+/*
+  Using Cors-anywhere; for dev purposes, requires asking for access here:
+    https://cors-anywhere.herokuapp.com/corsdemo
+  Read more here:
+    https://stackoverflow.com/questions/29670703/how-to-use-cors-anywhere-to-reverse-proxy-and-add-cors-headers
+*/
+
+
+function QuoteMap({Logout}) {
+  /*
+    List of:
+    { 
+      authorName: 'bob', 
+      coordinates: [lon,lat] 
+    }, ...
+
+    Note: S and W are negative lon and lat
+  */  
+  const [markers, setMarkers] = useState([])
+  const [country, setCountry] = useState('')
+  const [countries, setCountries] = useState([])
+
+  const handleDisplayClick = (e) => {
+    if (!countries.includes(country)) setCountries(oldCountries => [...oldCountries, country])
+  }
+
+  const handleClear = (e) => {
+    setCountries([])
+    // rerender markers
+  }
+
+  return (
+    <div className='Wrapper'>
+      <div className='navBarWrapper'>
+        <NavBar Logout={Logout}/>
+      </div>
+      
+      <h1> Explore quote authors on the interactive map! </h1>
+
+      <div className='SelectionDiv'>
+        <CountryDropdown value={country} onChange={(val) => {setCountry(val)}} />
+        <button onClick={handleDisplayClick}>
+          Display Authors!
+        </button>
+        <button onClick={handleClear}>
+          Clear
+        </button>
+      </div>
+
+      <h2> Currently displaying the authors from: {countries.join(', ')} </h2>
+
+      <div className='Map'>
+      <ComposableMap projection='geoMercator'>
+        <ZoomableGroup center={[0, 0]} zoom={1}>
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography 
+                  key={geo.rsmKey} 
+                  geography={geo} 
+                  fill='#429121' stroke='#282b28' strokeWidth={0.5}
+                />
+              ))
+            }
+          </Geographies>
+          {
+            markers.map(({authorName, coordinates}) => (
+              <Marker coordinates={coordinates}>
+              <a href='/authors/{authorName}'>
+                <circle r='1' fill='red' stroke='black' strokewidth={0.4}/>
+                <text textAnchor='middle' y={10} fontSize={7.5}> {authorName} </text>
+              </a>
+              </Marker>
+            ))
+          }
+        </ZoomableGroup>
+      </ComposableMap>
+    </div>
+    </div>
+  )
+}
+
+export default QuoteMap
